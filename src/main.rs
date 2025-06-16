d95f937e58792d806cad777cf776444fdf287927
damidorg <andigo1316@gmail.com>
2025-06-16 08:46:33 +0000

    added output

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use rand::Rng;
 use std::io;
-
 use std::io::Write;
 
 fn main() {
     eprintln!("write youre names");
-    print!("> ");
-    io::stdout().flush().expect("PROGRAM BROC");
-    let buffer = io::stdin();
-    let mut input = String::new();
-    buffer.read_line(&mut input).expect("bad input");
-    println!("{input}")
+
+    let mut names: Vec<String> = Vec::new();
+    loop {
+        loop {
+            print!("> ");
+            io::stdout().flush().expect("PROGRAM BROC");
+            let buffer = io::stdin();
+            let mut input = String::new();
+            buffer.read_line(&mut input).expect("bad input");
+
+            match input.trim() {
+                "done" => break,
+                "" => println!("enter a name!"),
+                _ => names.push(input.to_string()),
+            }
+        }
+        let randomnumber = rand::rng().random_range(..names.len());
+        println!("{}", names[randomnumber])
+    }
 }
