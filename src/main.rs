c3136a92e75d12d2b310465b0816514f93eb6288
damidorg <andigo1316@gmail.com>
2025-06-24 09:02:21 +0000

    last standing

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,41 @@ fn main() {
 
     let mut names: Vec<String> = Vec::new();
     loop {
-        loop {
-            print!("> ");
-            io::stdout().flush().expect("PROGRAM BROC");
-            let buffer = io::stdin();
-            let mut input = String::new();
-            buffer.read_line(&mut input).expect("bad input");
+        print!("> ");
+        io::stdout().flush().expect("PROGRAM BROC");
+        let buffer = io::stdin();
+        let mut input = String::new();
+        buffer.read_line(&mut input).expect("bad input");
 
-            match input.trim() {
-                "done" => break,
-                "" => println!("enter a name!"),
-                _ => names.push(input.to_string()),
-            }
+        match input.trim() {
+            "done" => break,
+            "" => println!("enter a name!"),
+            _ => names.push(input.to_string()),
         }
-        let randomnumber = rand::rng().random_range(..names.len());
-        println!("{}", names[randomnumber])
     }
+
+    print!("> ");
+    io::stdout().flush().expect("PROGRAM BROC");
+    let buffer = io::stdin();
+    let mut input = String::new();
+    buffer.read_line(&mut input).expect("bad input");
+
+    match input.trim() {
+        "1" => last_standing(names),
+        _ => names.push(input.to_string()),
+    }
+}
+fn last_standing(mut names: Vec<String>) {
+    loop {
+        let randomnumber = rand::rng().random_range(..names.len());
+        println!("{}", names[randomnumber]);
+        names.remove(randomnumber);
+        if names.len() < 1 {
+            break;
+        }
+    }
+}
+fn random_name(names: Vec<String>) {
+    let randomnumber = rand::rng().random_range(..names.len());
+    println!("{}", names[randomnumber])
 }
