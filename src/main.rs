fd4ef9f9abfd74741e6555fb28c33620ae5373f4
damidorg <andigo1316@gmail.com>
2025-06-24 09:12:31 +0000

    better q&a

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
         io::stdout().flush().expect("PROGRAM BROC");
         let buffer = io::stdin();
         let mut input = String::new();
-        buffer.read_line(&mut input).expect("bad input");
+        buffer.read_line(&mut input).expect("invalid input");
 
         match input.trim() {
             "done" => break,
@@ -19,7 +19,7 @@ fn main() {
             _ => names.push(input.to_string()),
         }
     }
-
+    println!("what mode?\n(1)last standing\n(2)random name ");
     print!("> ");
     io::stdout().flush().expect("PROGRAM BROC");
     let buffer = io::stdin();
@@ -28,14 +28,17 @@ fn main() {
 
     match input.trim() {
         "1" => last_standing(names),
+        "2" => random_name(names),
         _ => names.push(input.to_string()),
     }
 }
 fn last_standing(mut names: Vec<String>) {
+    let mut place: u32 = 1;
     loop {
         let randomnumber = rand::rng().random_range(..names.len());
-        println!("{}", names[randomnumber]);
+        println!("{}.{}", place, names[randomnumber]);
         names.remove(randomnumber);
+        place += 1;
         if names.len() < 1 {
             break;
         }
